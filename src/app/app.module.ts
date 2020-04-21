1f60555f26df67e03af843b6c122f3eaa72a5ca4
Pedro Bacchini <pedrobacchini@outlook.com>
2020-04-21 18:05:08 -0300

    Create Navigation.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,23 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatListModule } from "@angular/material/list";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/template/header/header.component';
 import { FooterComponent } from './components/template/footer/footer.component';
+import { NavComponent } from './components/template/nav/nav.component';
 
 @NgModule({
     declarations: [
         AppComponent,
         HeaderComponent,
-        FooterComponent
+        FooterComponent,
+        NavComponent
     ],
     imports: [
         BrowserModule,
         BrowserAnimationsModule,
 
         MatToolbarModule,
+        MatSidenavModule,
+        MatListModule,
 
         AppRoutingModule
     ],

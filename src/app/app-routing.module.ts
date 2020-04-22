cdd20067b9fa9b6b614be82c4a086328a51dd535
Pedro Bacchini <pedrobacchini@outlook.com>
2020-04-21 21:05:52 -0300

    Configure routing.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+
+import { ProductComponent } from "./views/product/product.component";
+import { HomeComponent } from "./views/home/home.component";
 
 
-const routes: Routes = [];
+const routes: Routes = [
+    {path: '', redirectTo: 'home', pathMatch: 'full'},
+    {path: "home", component: HomeComponent},
+    {path: "products", component: ProductComponent}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import { HeaderComponent } from './components/template/header/header.component';
 import { FooterComponent } from './components/template/footer/footer.component';
 import { NavComponent } from './components/template/nav/nav.component';
 import { HomeComponent } from './views/home/home.component';
+import { ProductComponent } from './views/product/product.component';
 
 @NgModule({
     declarations: [
@@ -20,7 +21,8 @@ import { HomeComponent } from './views/home/home.component';
         HeaderComponent,
         FooterComponent,
         NavComponent,
-        HomeComponent
+        HomeComponent,
+        ProductComponent
     ],
     imports: [
         BrowserModule,
